c3004f4f8ac8c5c4c84c756d749684bc83b235cf
superatmel <superatmel@gmail.com>
2012-04-18 08:19:46 +0200

    Edit locales for spanish
    Change-Id: I9905f0ec2f4b4071c3df0847791a0aad70c5d85c

--- sirius/osr_sirius.mk
+++ sirius/osr_sirius.mk
@@ -35,4 +35,4 @@ PRODUCT_PACKAGE_OVERLAYS += device/nvsbl/sirius/overlay
 # Inherit Flash Player
 -include vendor/osr/products/flashplayer.mk
 
-PRODUCT_LOCALES += hdpi
+PRODUCT_LOCALES := es_ES ca_ES zh_CN ja_JP ko_KR ru_RU en_US en_GB fr_FR it_IT de_DE hdpi
